916aa780a5112066d812f312b838dc763e0fd188
yashiiieieie <105464506+yashiiieieie@users.noreply.github.com>
2023-01-27 02:58:00 +0530

    Add files via upload

---
+++ src/components/pages/Abts.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Abt.css";
+
+function Abts() {
+  return (
+    <div className="about">
+      <h1>About Us</h1>
+      <p>Hello, wishes from ResumeBuilder Team</p>
+      <br />
+      <p>
+      <h3><u>Team members </u></h3>
+      <br/>
+      <h4><li>Anurag Chaturvedi  &nbsp;[Roll No. - 19T7010] </li></h4>
+
+      </p>
+      <br />
+      <h4><li>Yashi Gupta &nbsp;[Roll No. - 19T7072] </li></h4>
+      <br />
+      <h4><li>Arushi Patidar &nbsp;[Roll No. - 19T7011] </li></h4>
+      <br />
+      <h4><li>Yash Chouhan &nbsp;[Roll No. - 19T7071] </li></h4>
+    
+      
+      
+    </div>
+  );
+}
+
+export default Abts;
